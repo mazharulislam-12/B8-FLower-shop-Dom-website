50ca99fceb4768f6b60777c53fb52ee863bdc2da
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-16 12:02:57 -0700

    app . js file

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-
+let total = 0;  
 function handleCLikBtn(target) {
-    
+    const selectedItemContainer = document.getElementById('selected-items');
+    const itemName = target.parentNode.childNodes[1].innerText;
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    selectedItemContainer.appendChild(li);
+    const price = target.parentNode.childNodes[5].innerText.split(' ')[1];
+    total = parseInt(total) + parseInt(price);
+    document.getElementById('total').innerText =    total;
+
 }
